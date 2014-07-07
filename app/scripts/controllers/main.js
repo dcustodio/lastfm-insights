8b148f43a2253bb64d839500b6ed5dc6849b265c
David Custodio <davidlacustodio@gmail.com>
2014-07-07 23:43:47 +0100

    get artists and tags

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
-app.controller('MainCtrl',['$scope','userService', function ($scope, userService) {
+app.controller('MainCtrl',['$scope','userService', function ($scope,userService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-        $scope.artists = userService.user;
+    $scope.tags = [];
+//$scope.artists = userService.getArtists();
+    $scope.getTags = function(){
+        $scope.tags = userService.getTags($scope.username);
+    }
   }]);
--- app/scripts/services/userService.js
+++ app/scripts/services/userService.js
@@ -1,16 +1,82 @@
 /**
  * Created by David on 23/01/14.
  */
-var services = angular.module('userService', []).
+'use strict';
+
+var services = angular.module('lastfmInsightsApp.services', []).
     value('version', '0.1');
 
+//get user.getWeeklyTrackChart
+//get top tags
 services.service('userService', ['$http', '$q', function ($http, $q) {
     var service = {};
+    var get_params = { method:"user.gettopartists",
+        user:"rj",
+        api_key:"1a19451013ef6665afd0a24e64422227",
+        format:"jon" };
+/*
+    service.getArtists = function(){
+        Restangular.all('accounts');
 
-    //http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=rj&api_key=1a19451013ef6665afd0a24e64422227&format=json
+        console.log("getting artists");
+        return Restangular.all().getList(get_params);
+    } */
+    service.getWeeklyTrackChart = function (username) {
+        var deferred = $q.defer();
+
+        $http.get('http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&user='+username+'&api_key=1a19451013ef6665afd0a24e64422227&format=json').
+            success(function (data) {
+                deferred.resolve(data);
+            }).
+            error(function (error) {
+                deferred.reject(error.message);
+            });
+
+        return deferred.promise;
+    };
+
+    service.getTags = function(username) {
+
+        var tags = [];
+        service.getWeeklyTrackChart(username).then(function(res){
+
+            res.weeklytrackchart.track.forEach(function(tr) {
+                service.getArtistTopTags(tr.artist['#text']).then(function(tag){
+                    tag.toptags.tag.forEach(function(t){
+                        tags.push(t);
+                    })
+                });
+            });
+            return tags;
+        });
+
+
+
+    };
+
+service.getArtistTopTags = function(artist) {
+    var deferred = $q.defer();
+
+    $http.get('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist='+artist+'&api_key=1a19451013ef6665afd0a24e64422227&format=json').
+        success(function (data) {
+            deferred.resolve(data);
+        }).
+        error(function (error) {
+            deferred.reject(error.message);
+        });
+
+    return deferred.promise;
+}
+
+
+//return service.artists = artists;
+
+
+
+    //http://ws.audioscrobbler.com/2.0/?=&&&
     //Restangular
-    var User = $resource('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=rj&api_key=1a19451013ef6665afd0a24e64422227&format=json');
-    var user = User.get( function() {
+    //var User = $resource('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=rj&api_key=1a19451013ef6665afd0a24e64422227&format=json');
+    /*var user = User.get( function() {
 
     });
 
@@ -32,7 +98,9 @@ services.service('userService', ['$http', '$q', function ($http, $q) {
 
     service.getUserById = function (userid) {
         return getUser(userid);
-    }
+    }*/
+
+
 
     return service;
 }]);
